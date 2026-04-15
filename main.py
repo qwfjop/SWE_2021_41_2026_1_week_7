cb021c535c8c3bf8b036ebb1b1358b632cf3326f
Jonghyeok Ryu <ryujonghyeok@gmail.com>
2026-04-15 18:43:41 +0900

    Initial version of main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if nums[i] + nums[j] == target:
+                return f"[{i}, {j}]"
